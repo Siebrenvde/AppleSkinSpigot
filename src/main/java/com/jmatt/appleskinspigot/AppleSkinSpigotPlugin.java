93b55d1618670c376a8d3a27cbc577815e2eba1e
Siebrenvde <siebren@siebrenvde.dev>
2025-03-15 06:53:26 +0100

    Support naturalRegeneration game rule syncing

--- src/main/java/com/jmatt/appleskinspigot/AppleSkinSpigotPlugin.java
+++ src/main/java/com/jmatt/appleskinspigot/AppleSkinSpigotPlugin.java
@@ -1,17 +1,23 @@
 package com.jmatt.appleskinspigot;
 
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.messaging.Messenger;
 
 public class AppleSkinSpigotPlugin extends JavaPlugin {
 
+    public static AppleSkinSpigotPlugin INSTANCE;
+
     public static String SATURATION_KEY = "appleskin:saturation";
     public static String EXHAUSTION_KEY = "appleskin:exhaustion";
+    public static final String NATURAL_REGENERATION_KEY = "appleskin:natural_regeneration";
 
     private SyncTask syncTask = null;
 
     @Override
     public void onEnable() {
+        INSTANCE = this;
+
         String[] version = getServer().getBukkitVersion().split("-")[0].split("\\.");
         int minor = Integer.parseInt(version[1]);
         int patch = version.length > 2 ? Integer.parseInt(version[2]) : 0;
@@ -21,11 +27,19 @@ public class AppleSkinSpigotPlugin extends JavaPlugin {
             EXHAUSTION_KEY = "appleskin:exhaustion_sync";
         }
 
-        getServer().getPluginManager().registerEvents(new LoginListener(this), this);
+        PluginManager manager = getServer().getPluginManager();
+        manager.registerEvents(new LoginListener(this), this);
 
         Messenger messenger = getServer().getMessenger();
         messenger.registerOutgoingPluginChannel(this, SATURATION_KEY);
         messenger.registerOutgoingPluginChannel(this, EXHAUSTION_KEY);
+
+        // https://github.com/squeek502/AppleSkin/commit/ee4d5316138a5ab4b2406753d4ed717e12f550dd
+        if (minor > 21 || (minor == 21 && patch >= 3)) {
+            messenger.registerOutgoingPluginChannel(this, NATURAL_REGENERATION_KEY);
+            manager.registerEvents(new GameRuleListener(), this);
+            if (isPaper()) manager.registerEvents(new GameRuleListener.Paper(), this);
+        }
     }
 
     /**
@@ -59,6 +73,23 @@ public class AppleSkinSpigotPlugin extends JavaPlugin {
     @Override
     public void onDisable() {
         cancelSyncTask();
+    }
+
+    /**
+     * Checks whether the current server is a Paper server
+     * @return <code>true</code> if the server is a Paper server
+     */
+    private boolean isPaper() {
+        try {
+            Class.forName("io.papermc.paper.configuration.Configuration");
+            return true;
+        } catch (ClassNotFoundException ignored) {
+            try {
+                Class.forName("com.destroystokyo.paper.PaperConfig");
+                return true;
+            } catch (ClassNotFoundException ignored1) {}
+        }
+        return false;
     }
 
 }
---
+++ src/main/java/com/jmatt/appleskinspigot/GameRuleListener.java
@@ -0,0 +1,58 @@
+package com.jmatt.appleskinspigot;
+
+import io.papermc.paper.event.world.WorldGameRuleChangeEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.GameRule;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.nio.ByteBuffer;
+
+public class GameRuleListener implements Listener {
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Bukkit.getScheduler().runTaskLater(
+            AppleSkinSpigotPlugin.INSTANCE,
+            () -> { if (event.getPlayer().isOnline()) sendNaturalRegenState(event.getPlayer()); },
+            20L
+        );
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerChangedWorld(PlayerChangedWorldEvent event) {
+        sendNaturalRegenState(event.getPlayer());
+    }
+
+    private void sendNaturalRegenState(Player player) {
+        sendNaturalRegenState(
+            player,
+            Boolean.TRUE.equals(player.getWorld().getGameRuleValue(GameRule.NATURAL_REGENERATION))
+        );
+    }
+
+    private static void sendNaturalRegenState(Player player, boolean state) {
+        player.sendPluginMessage(
+            AppleSkinSpigotPlugin.INSTANCE,
+            AppleSkinSpigotPlugin.NATURAL_REGENERATION_KEY,
+            ByteBuffer.allocate(1).put((byte) (state ? 1 : 0)).array()
+        );
+    }
+
+    public static class Paper implements Listener {
+
+        @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+        public void onGameRuleChange(WorldGameRuleChangeEvent event) {
+            if (event.getGameRule() != GameRule.NATURAL_REGENERATION) return;
+            event.getWorld().getPlayers().forEach(player -> {
+                sendNaturalRegenState(player, Boolean.parseBoolean(event.getValue()));
+            });
+        }
+
+    }
+
+}
