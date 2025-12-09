9bdbd5eb852ef6285986125f4fe97bf0e24df3ad
Siebrenvde <siebren@siebrenvde.dev>
2025-12-09 19:24:41 +0100

    Update to 1.21.11

--- src/main/java/com/jmatt/appleskinspigot/listeners/GameRuleListener.java
+++ src/main/java/com/jmatt/appleskinspigot/listeners/GameRuleListener.java
@@ -1,8 +1,12 @@
 package com.jmatt.appleskinspigot.listeners;
 
 import com.jmatt.appleskinspigot.AppleSkinSpigot;
+import com.jmatt.appleskinspigot.util.ServerVersion;
 import io.papermc.paper.event.world.WorldGameRuleChangeEvent;
 import org.bukkit.GameRule;
+import org.bukkit.GameRules;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Registry;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -15,6 +19,21 @@ import java.nio.ByteBuffer;
 import static com.jmatt.appleskinspigot.AppleSkinSpigot.NATURAL_REGENERATION_KEY;
 
 public final class GameRuleListener implements Listener {
+
+    private static final GameRule<Boolean> NATURAL_REGENERATION = getGameRule();
+
+    @SuppressWarnings({"removal", "unchecked"})
+    private static GameRule<Boolean> getGameRule() {
+        if (ServerVersion.isHigherThanOrEqualTo(21, 11)) {
+            if (AppleSkinSpigot.isPaper()) {
+                return GameRules.NATURAL_HEALTH_REGENERATION;
+            } else {
+                return (GameRule<Boolean>) Registry.GAME_RULE.getOrThrow(NamespacedKey.minecraft("natural_health_regeneration"));
+            }
+        } else {
+            return GameRule.NATURAL_REGENERATION;
+        }
+    }
 
     @EventHandler
     private void onPlayerRegisterChannel(final PlayerRegisterChannelEvent event) {
@@ -33,7 +52,7 @@ public final class GameRuleListener implements Listener {
     private void sendNaturalRegenState(final Player player) {
         sendNaturalRegenState(
             player,
-            Boolean.TRUE.equals(player.getWorld().getGameRuleValue(GameRule.NATURAL_REGENERATION))
+            Boolean.TRUE.equals(player.getWorld().getGameRuleValue(NATURAL_REGENERATION))
         );
     }
 
@@ -49,7 +68,7 @@ public final class GameRuleListener implements Listener {
 
         @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
         public void onGameRuleChange(final WorldGameRuleChangeEvent event) {
-            if (event.getGameRule() != GameRule.NATURAL_REGENERATION) return;
+            if (event.getGameRule() != NATURAL_REGENERATION) return;
             event.getWorld().getPlayers().forEach(player -> {
                 if (player.getListeningPluginChannels().contains(NATURAL_REGENERATION_KEY)) {
                     sendNaturalRegenState(player, Boolean.parseBoolean(event.getValue()));
